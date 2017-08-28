78e49ef06568d6886443e694861cab6b1fbb250d
Andrey Gluzdov <adgluzdov@mail.ru>
2017-08-28 21:59:51 +0300

    Hello World

---
+++ application/app.go
@@ -0,0 +1,7 @@
+package application
+
+import "fmt"
+
+func Test() {
+	fmt.Printf("Hello World")
+}
--- imoby.go
+++ imoby.go
@@ -1,5 +1,7 @@
 package main
 
-func main()  {
+import "github.com/adgluzdov/IMobyServer/application"
 
+func main()  {
+	application.Test()
 }
